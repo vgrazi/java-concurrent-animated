a6098c36dcd4a766b9e6b9a216cde6274f15a2fb
vgrazi <vgrazi@1fce8219-d888-4380-8bb7-1b23191ce554>
2010-05-23 08:07:32 +0000

    Initial revision

---
+++ src/vgrazi/concurrent/samples/examples/Pooled.java
@@ -0,0 +1,12 @@
+package vgrazi.concurrent.samples.examples;
+
+/**
+ * Used by the ConcurrentSpriteCanvas to determine pool size
+ */
+public interface Pooled {
+  /**
+   * Returns the number of available threads in the pool
+   * @return
+   */
+  int getAvailableThreadCount();
+}
