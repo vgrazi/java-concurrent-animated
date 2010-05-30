623d7d275dd04baad6cc1854a1a3c78954e8e3e1
vgrazi <vgrazi@1fce8219-d888-4380-8bb7-1b23191ce554>
2010-05-30 12:05:46 +0000

    Added a finale

--- src/vgrazi/concurrent/samples/examples/LegendExample.java
+++ src/vgrazi/concurrent/samples/examples/LegendExample.java
@@ -7,25 +7,18 @@ import vgrazi.concurrent.samples.sprites.ConcurrentTextSprite;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Date;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.logging.Logger;
 
 /*
  * @user vgrazi.
  * Time: 12:26:11 AM
  */
 public class LegendExample extends ConcurrentExample {
-  private final static Logger logger = Logger.getLogger(LegendExample.class.getCanonicalName());
-  private ReadWriteLock lock;
   private final Object MUTEX = new Object();
   int position = 0;
   private final JButton startButton = new JButton("Start Demo");
 
   private boolean initialized = false;
-  private static int minSnippetPosition = 390;
+  private static int minSnippetPosition = 500;
 
   public LegendExample(String label, Container frame, int slideNumber) {
     super(label, frame, ExampleType.BLOCKING, minSnippetPosition, false, slideNumber);
@@ -60,6 +53,8 @@ public class LegendExample extends ConcurrentExample {
   private void startDemo() {
     switch (position) {
       case 0:
+        // reset just in case we are coming around again
+        resetExample();
         // todo: add justification - left, right, center.
         sprites[0] = createTextSprite("This is");
         sprites[6] = createTextSprite("a block");
@@ -126,16 +121,25 @@ public class LegendExample extends ConcurrentExample {
         break;
       case 9:
         ((ConcurrentTextSprite) sprites[17]).setText("Released");
+        sprites[15].setReleased();
         sprites[16].setReleased();
         sprites[17].setReleased();
         position++;
+        break;
+      case 10:
+        sprites[18] = createTextSprite("Page Down");
+        sprites[19] = createTextSprite("To start");
+        sprites[20] = createTextSprite("presentation");
+        sprites[18].setAcquired();
+        sprites[19].setAcquired();
+        sprites[20].setAcquired();
+        position = 0;
         break;
     }
   }
 
   protected void reset() {
     resetExample();
-//    lock = new ReentrantReadWriteLock(isFair());
     position = 0;
   }
 
@@ -151,55 +155,10 @@ public class LegendExample extends ConcurrentExample {
     message2("  ", ConcurrentExampleConstants.DEFAULT_BACKGROUND);
   }
 
+  // todo: add snippet explaining how the snippets work
   protected String getSnippet() {
     String snippet;
-    snippet = "<html>" +
-            "<PRE> " +
-            "    <FONT style=\"font-family:monospaced;\" COLOR=\"#606060\"><I>// Construct the ReadWriteLock</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000080>\"><B>final</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> ReadWriteLock lock =  \n" +
-            "        </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000080>\"><B>new</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> ReentrantReadWriteLock(); \n" +
-            "        </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state6:#000080>\"><B>new</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state6:#000000>\"> ReentrantReadWriteLock(true); \n" +
-            //       "        </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000080>\"><B>new</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> WriterPreferenceReadWriteLock(); \n" +
-            //       "        </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000080>\"><B>new</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> ReaderPreferenceReadWriteLock(); \n" +
-            //       "        </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000080>\"><B>new</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> FIFOReadWriteLock(); \n" +
-            "     \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// Acquire the read lock</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\"> \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000080>\"><B>try</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\"> { \n" +
-            "      lock.readLock().lock(); \n" +
-            "      </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// or</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\"> \n" +
-            "      lock.readLock().tryLock(</FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000099>\">1000L, TimeUnit.MILLISECONDS</FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\">); \n" +
-            "    }" +
-            " </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000080>\"><B>catch</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\">(InterruptedException e) { }\n" +
-            //            "      Thread.currentThread().interrupt(); \n" +
-            //            "    } \n" +
-            " \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state2:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// Release the read lock</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state2:#000000>\"> \n" +
-            "    lock.readLock().unlock(); \n" +
-            " \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state2:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// Acquire the write lock</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\"> \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000080>\"><B>try</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\"> { \n" +
-            "      lock.writeLock().lock(); \n" +
-            "      </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// or</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\"> \n" +
-            "      lock.writeLock().tryLock(</FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000099>\">1000L, TimeUnit.MILLISECONDS</FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\">); \n" +
-            "    }" +
-            " </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000080>\"><B>catch</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\">(InterruptedException e) { }\n" +
-            //            "    } \n" +
-            " \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state4:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// Release the lock</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state4:#000000>\"> \n" +
-            "    lock.writeLock().unlock(); \n" +
-            "    // or \n" +
-            "    lock.readLock().unlock(); \n" +
-            " \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state5:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\"><I>// Downgrade the write lock</I></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\"> \n" +
-            "    </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state5:#000080>\"><B>try</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state5:#000000>\"> { \n" +
-            "      lock.readLock().lock(); \n" +
-            "      lock.writeLock().unlock();\n" +
-            "    }" +
-            " </FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state5:#000080>\"><B>catch</B></FONT><FONT style=\"font-family:monospaced;\" COLOR=\"<state5:#000000>\">(InterruptedException e) {} \n" +
-            //            "    } \n" +
-            " \n" +
-            "</FONT>" +
-            "</PRE></html>";
+    snippet = "";
     return snippet;
   }
 }
