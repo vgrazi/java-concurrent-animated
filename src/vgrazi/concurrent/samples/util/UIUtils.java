379839062e7ec66fdf3818540046aec4bb207fdd
vgrazi <vgrazi@1fce8219-d888-4380-8bb7-1b23191ce554>
2011-07-04 00:16:33 +0000

    Moved all methods from vgrazi.util.UIUtils to vgrazi.concurrent.samples.util.UIUtils

--- src/vgrazi/concurrent/samples/util/UIUtils.java
+++ src/vgrazi/concurrent/samples/util/UIUtils.java
@@ -29,6 +29,13 @@ public class UIUtils {
     component.setBounds(0, 0, width, height);
   }
 
+  public static Frame getParentFrame(Component c) {
+    while (! (c instanceof Frame) && c!= null) {
+      c = c.getParent();
+    }
+    return ((Frame) c);
+  }
+
   private static Dimension getScreenSize() {
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     int screenWidth = screenSize.width;
@@ -47,5 +54,32 @@ public class UIUtils {
 //    logger.log(Level.INFO, "ConcurrentExampleLauncher.showTitlePane image: " + url);
     ImageIcon imageIcon = new ImageIcon(url);
     return imageIcon;
+  }
+
+  /**
+   * If less than 0, returns 0. If greater than 1 returns 1, else returns the value
+   * @param percent the percent value to coerce
+   * @return the value between 0 and 1
+   */
+  public static float coerceZeroToOne(float percent) {
+    if(percent <= 0) return 0; else if (percent > 1) return 1; else return percent;
+  }
+
+  /**
+   * Sets the size as a percentage of the screen width and height, then centers it
+   * @param frame the frame to center
+   * @param widthPercent the percent of the screen width to size it
+   * @param heightPercent the percent of the screen height to size it
+   */
+  public static void center(Frame frame, float widthPercent, float heightPercent) {
+    widthPercent = coerceZeroToOne(widthPercent);
+    heightPercent = coerceZeroToOne(heightPercent);
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int width = (int) (widthPercent * screenSize.width);
+    int height = (int) (heightPercent * screenSize.height);
+    int x = (screenSize.width - width)/2;
+    int y = (screenSize.height - height)/2;
+    frame.setBounds(x, y, width, height);
   }
 }
