7a06e45bf0ecf9838ad9c0c64815b7b52a680653
vgrazi <vgrazi@1fce8219-d888-4380-8bb7-1b23191ce554>
2012-05-17 14:30:25 +0000

    Added more conditions

--- src/vgrazi/concurrent/samples/examples/ConditionExample.java
+++ src/vgrazi/concurrent/samples/examples/ConditionExample.java
@@ -1,6 +1,5 @@
 package vgrazi.concurrent.samples.examples;
 
-import vgrazi.concurrent.samples.examples.ConcurrentExample;
 import vgrazi.concurrent.samples.ConcurrentExampleConstants;
 import vgrazi.concurrent.samples.ExampleType;
 import vgrazi.concurrent.samples.sprites.ConcurrentSprite;
@@ -16,7 +15,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 
 /**
- * Lock/Condition parallel synchronized/wait/notify. In fact, if there is only one condition, it makes sense to use the low level sematics.
+ * Lock/Condition parallel synchronized/wait/notify. In fact, if there is only one condition, it makes sense to use the low level semantics.
  * However once more than one condition is being tested, it makes sense to consider using a Condition.
  * Otherwise, you might have one lock object requiring many notifies.
  * Consider naming each Condition variable after the condition it is testing, for example below, notFullCondition
@@ -25,14 +24,32 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ConditionExample extends ConcurrentExample {
 
   private ReentrantLock lock;
-  private Condition condition;
+  private Condition condition1;
+  private Condition condition2;
+  private Condition condition3;
+  private final static Color CONDITION2_COLOR = Color.CYAN;
+  private final static Color CONDITION3_COLOR = Color.GREEN;
+  private static final Color CONDITION1_SIGNAL_COLOR = Color.blue.brighter();
+  private static final Color CONDITION2_SIGNAL_COLOR = CONDITION2_COLOR.brighter();
+  private static final Color CONDITION3_SIGNAL_COLOR = CONDITION3_COLOR.brighter().brighter();
 
-  private final JButton awaitButton = new JButton("await");
-  private final JButton signalButton = new JButton("signal");
-  private final JButton signalAllButton = new JButton("signalAll");
+  /**
+   * Used for the snippet rendering, keeps track of which condition was last signalled - 1, 2, or 3
+   */
+  private int conditionIndex = 1;
+
+  private final JButton await1Button = new JButton("await [for condition 1]");
+  private final JButton await2Button = new JButton("await [for condition 2]");
+  private final JButton await3Button = new JButton("await [for condition 3]");
+  private final JButton signal1Button = new JButton("signal [condition 1]");
+  private final JButton signal2Button = new JButton("signal [condition 2]");
+  private final JButton signal3Button = new JButton("signal [condition 3]");
+  private final JButton signal1AllButton = new JButton("signalAll [condition 1]");
+  private final JButton signal2AllButton = new JButton("signalAll [condition 2]");
+  private final JButton signal3AllButton = new JButton("signalAll [condition 3]");
 
   private boolean initialized = false;
-  private static final int MIN_SNIPPET_POSITION = 600;
+  private static final int MIN_SNIPPET_POSITION = 700;
 
   public ConditionExample(String title, Container frame, int slideNumber) {
     super(title, frame, ExampleType.WORKING, MIN_SNIPPET_POSITION, false, slideNumber);
@@ -41,13 +58,14 @@ public class ConditionExample extends ConcurrentExample {
 
   protected void initializeComponents() {
     if(!initialized) {
-      initializeButton(awaitButton, new Runnable() {
+      initializeButton(await1Button, new Runnable() {
         public void run() {
           try {
+            conditionIndex = 1;
             setState(1);
             ConcurrentSprite sprite = createAcquiringSprite();
             lock.lock();
-            condition.await();
+            condition1.await();
 //            setState(2);
             sprite.setReleased();
           }
@@ -59,29 +77,132 @@ public class ConditionExample extends ConcurrentExample {
           }
         }
       });
-      initializeButton(signalButton, new Runnable() {
+      initializeButton(await2Button, new Runnable() {
         public void run() {
-          createReleasingSprite();
+          try {
+            conditionIndex = 2;
+            setState(1);
+            ConcurrentSprite sprite = createAcquiringSprite();
+            sprite.setColor(CONDITION2_COLOR);
+            lock.lock();
+            condition2.await();
+//            setState(2);
+            sprite.setReleased();
+          }
+          catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+          finally {
+            lock.unlock();
+          }
+        }
+      });
+      initializeButton(await3Button, new Runnable() {
+        public void run() {
+          try {
+            conditionIndex = 3;
+            setState(1);
+            ConcurrentSprite sprite = createAcquiringSprite();
+            sprite.setColor(CONDITION3_COLOR);
+            lock.lock();
+            condition3.await();
+//            setState(2);
+            sprite.setReleased();
+          }
+          catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+          finally {
+            lock.unlock();
+          }
+        }
+      });
+      addButtonSpacer();
+      initializeButton(signal1Button, new Runnable() {
+        public void run() {
+          ConcurrentSprite sprite = createReleasingSprite();
+          sprite.setColor(CONDITION1_SIGNAL_COLOR);
+          conditionIndex = 1;
           setState(2);
           lock.lock();
-          condition.signal();
+          condition1.signal();
           lock.unlock();
         }
       });
-      initializeButton(signalAllButton, new Runnable() {
+      initializeButton(signal2Button, new Runnable() {
         public void run() {
-          createReleasingSprite();
+          conditionIndex = 2;
+          ConcurrentSprite sprite = createReleasingSprite();
+          sprite.setColor(CONDITION2_SIGNAL_COLOR);
+          setState(2);
+          lock.lock();
+          condition2.signal();
+          lock.unlock();
+        }
+      });
+      initializeButton(signal3Button, new Runnable() {
+        public void run() {
+          conditionIndex = 3;
+          ConcurrentSprite sprite = createReleasingSprite();
+          sprite.setColor(CONDITION3_SIGNAL_COLOR);
+          setState(2);
+          lock.lock();
+          condition3.signal();
+          lock.unlock();
+        }
+      });
+      addButtonSpacer();
+      initializeButton(signal1AllButton, new Runnable() {
+        public void run() {
+          conditionIndex = 1;
+          ConcurrentSprite sprite = createReleasingSprite();
+          sprite.setColor(CONDITION1_SIGNAL_COLOR);
           setState(3);
           lock.lock();
-          condition.signalAll();
+          condition1.signalAll();
           lock.unlock();
         }
       });
+      initializeButton(signal2AllButton, new Runnable() {
+        public void run() {
+          conditionIndex = 2;
+          ConcurrentSprite sprite = createReleasingSprite();
+          sprite.setColor(CONDITION2_SIGNAL_COLOR);
+          setState(3);
+          lock.lock();
+          condition2.signalAll();
+          lock.unlock();
+        }
+      });
+      initializeButton(signal3AllButton, new Runnable() {
+        public void run() {
+          conditionIndex = 3;
+          ConcurrentSprite sprite = createReleasingSprite();
+          sprite.setColor(CONDITION3_SIGNAL_COLOR);
+          setState(3);
+          lock.lock();
+          condition3.signalAll();
+          lock.unlock();
+        }
+      });
+      Dimension size = new Dimension(180, 32);
+      await1Button.setPreferredSize(size);
+      await2Button.setPreferredSize(size);
+      await3Button.setPreferredSize(size);
+      signal1Button.setPreferredSize(size);
+      signal2Button.setPreferredSize(size);
+      signal3Button.setPreferredSize(size);
+      signal1AllButton.setPreferredSize(size);
+      signal2AllButton.setPreferredSize(size);
+      signal3AllButton.setPreferredSize(size);
       initialized = true;
     }
   }
 
-
+  @Override
+  public int getVerticalOffsetShift() {
+    return -60;
+  }
 
   public String getDescriptionHtml() {
     StringBuffer sb = new StringBuffer();
@@ -110,7 +231,10 @@ public class ConditionExample extends ConcurrentExample {
   @Override
   public void reset() {
     lock = new ReentrantLock();
-    condition = lock.newCondition();
+    conditionIndex = 1;
+    condition1 = lock.newCondition();
+    condition2 = lock.newCondition();
+    condition3 = lock.newCondition();
     message1(" ", ConcurrentExampleConstants.MESSAGE_COLOR);
     message2(" ", ConcurrentExampleConstants.MESSAGE_COLOR);
     setState(0);
@@ -118,11 +242,16 @@ public class ConditionExample extends ConcurrentExample {
   }
 
   protected String getSnippet() {
+    return getBaseSnippet().replaceAll("\\[i\\]", String.valueOf(conditionIndex));
+  }
+  protected String getBaseSnippet() {
 
     return "<html><PRE><font 'style=\"font-family:monospaced;\" COLOR=\"" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + "\">" +
 //       "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + "\"><I>// Constructor</I></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"#000000\"> \n" +
        "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"><B>Lock lock = </B></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"><B>new</B></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> ReentrantLock(); \n" +
-       "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"><B>Condition condition = </B></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> lock.newCondition(); \n" +
+       "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"><B>Condition condition1 = </B></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> lock.newCondition(); \n" +
+       "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"><B>Condition condition2 = </B></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> lock.newCondition(); \n" +
+       "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"><B>Condition condition3 = </B></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"></FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state0:#000000>\"> lock.newCondition(); \n" +
        "\n" +
 //       "    </FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state2:" + ConcurrentExampleConstants.HTML_DISABLED_COLOR + ">\">\n" +
 //       "</FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state2:#000000>\"> \n" +
@@ -132,9 +261,9 @@ public class ConditionExample extends ConcurrentExample {
        "</FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\"> \n" +
        "    lock.lock();\n" +
        "    try {\n" +
-       "      condition.await(); \n" +
-       "    } catch(InterruptedException e) { interrupt(); }\n" +
-       "</FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state2:#000000>\"> \n" +
+       "      condition[i].await(); \n" +
+       "    } catch(InterruptedException e) {...}\n" +
+       "</FONT><font 'style=\"font-family:monospaced;\" COLOR=\"<state1:#000000>\"> \n" +
        "    finally {\n" +
        "      lock.unlock();\n" +
        "    } \n" +
@@ -142,13 +271,13 @@ public class ConditionExample extends ConcurrentExample {
             "\n" +
        "<font 'style=\"font-family:monospaced;\" COLOR=\"<state2:#000000>\"> \n" +
        "    lock.lock();\n" +
-       "    condition.signal();\n" +
+       "    condition[i].signal();\n" +
        "    lock.unlock();\n" +
        "</FONT>" +
        "\n" +
             "<font 'style=\"font-family:monospaced;\" COLOR=\"<state3:#000000>\"> \n" +
        "    lock.lock();\n" +
-       "    condition.signalAll();\n" +
+       "    condition[i].signalAll();\n" +
        "    lock.unlock();\n" +
        "</FONT>" +
        "</PRE></html>";
